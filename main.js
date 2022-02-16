aa8ae8770afa541530bb8c62ca80b83e38325eea
Symbiosis <luka.tarman@gmail.com>
2022-02-16 12:58:09 +0100

    My first commit.
    Commited my first git change. Added console log test.

---
+++ main.js
@@ -0,0 +1,10 @@
+import { Triangle } from "./triangle-area-calculate.js";
+
+const sTriangle = new Triangle(3, 6, 7);
+const eTriangle = new Triangle(5, 5, 5);
+const iTriangle = new Triangle(6, 4, 6);
+
+console.log("test");
+console.log(sTriangle.calculateArea());
+
+console.log("test number two");
