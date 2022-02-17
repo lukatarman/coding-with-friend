2a4ae50e95b346b1f0c7522fbedb30092b7b1574
Symbiosis <luka.tarman@gmail.com>
2022-02-17 11:55:41 +0100

    Remove test console logs from main.js

--- main.js
+++ main.js
@@ -4,7 +4,4 @@ const sTriangle = new Triangle(3, 6, 7);
 const eTriangle = new Triangle(5, 5, 5);
 const iTriangle = new Triangle(6, 4, 6);
 
-console.log("test");
 console.log(sTriangle.calculateArea());
-
-console.log("test number two");
