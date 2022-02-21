a3536c5aa66501b9f01d844513190700efc35f24
Luka Tarman <luka.tarman@gmail.com>
2022-02-21 11:46:19 +0100

    Added console log function to test, and running it in main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import { Triangle } from "./triangle-area-calculate.js";
+import { myTest } from "./testing.js";
 
 const sTriangle = new Triangle(3, 6, 7);
 const eTriangle = new Triangle(5, 5, 5);
@@ -8,3 +9,4 @@ console.log(sTriangle.calculateArea());
 
 console.log("Git testing");
 console.log("Another git test");
+myTest();
---
+++ testing.js
@@ -0,0 +1,3 @@
+export function myTest() {
+  console.log("HELLO!");
+}
