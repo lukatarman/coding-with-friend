bb7736afde2da88e9d5ee7fef55f582fe310e6ea
Luka Tarman <luka.tarman@gmail.com>
2022-02-22 11:12:53 +0100

    Added right triangle check to main.js

--- main.js
+++ main.js
@@ -3,8 +3,10 @@ import { Triangle } from "./triangle.js";
 const sTriangle = new Triangle(3, 6, 7);
 const eTriangle = new Triangle(5, 5, 5);
 const iTriangle = new Triangle(6, 4, 6);
+const rTriangle = new Triangle(5, 3, 4);
 
 console.log(sTriangle.calculateArea());
+rTriangle.calculateArea();
 
 console.log("Git testing");
 console.log("Another git test");
